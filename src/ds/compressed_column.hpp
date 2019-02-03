53b1b6f634e717fb7f380db535abfa59cd7f905d
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-03 15:07:45 -0500

    Update src/ds/compressed_column.hpp

--- src/ds/compressed_column.hpp
+++ src/ds/compressed_column.hpp
@@ -306,53 +306,57 @@ TCSC_BASE<Weight, Integer_Type>::TCSC_BASE(uint64_t nnz_, Integer_Type nnzcols_,
         //printf("socket_id = %d nnz=%lu\n", socket_id, nnz);
         //send_buffer[i][s_i] = (MessageBuffer*)numa_alloc_onnode( sizeof(MessageBuffer), s_i);
         
-        #ifdef HAS_WEIGHT
-        A = (Weight*) numa_alloc_onnode(nnz * sizeof(Weight), socket_id);
-        memset(A, 0, nnz * sizeof(Weight));
-        #endif
-        IA = (Integer_Type*) numa_alloc_onnode(nnz * sizeof(Integer_Type), socket_id);
-        memset(IA, 0, nnz * sizeof(Integer_Type));
-        JA = (Integer_Type*) numa_alloc_onnode((nnzcols + 1) * sizeof(Integer_Type), socket_id);
-        memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
-        
+        if(numa_available() != -1) {
+            #ifdef HAS_WEIGHT
+            A = (Weight*) numa_alloc_onnode(nnz * sizeof(Weight), socket_id);
+            memset(A, 0, nnz * sizeof(Weight));
+            #endif
+            IA = (Integer_Type*) numa_alloc_onnode(nnz * sizeof(Integer_Type), socket_id);
+            memset(IA, 0, nnz * sizeof(Integer_Type));
+            JA = (Integer_Type*) numa_alloc_onnode((nnzcols + 1) * sizeof(Integer_Type), socket_id);
+            memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
+        } 
+        else {
+            #ifdef HAS_WEIGHT
+            if((A = (Weight*) mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+                fprintf(stderr, "Error mapping memory\n");
+                exit(1);
+            }
+            memset(A, 0, nnz * sizeof(Weight));
+            //numa_tonode_memory(A, nnz * sizeof(Weight), socket_id);
+            #endif
+            
+            
+            if((IA = (Integer_Type*) mmap(nullptr, nnz * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+                fprintf(stderr, "Error mapping memory\n");
+                exit(1);
+            }
+            memset(IA, 0, nnz * sizeof(Integer_Type));
+            //numa_tonode_memory(IA, nnz * sizeof(Integer_Type), socket_id);
+            
+            if((JA = (Integer_Type*) mmap(nullptr, (nnzcols + 1) * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+                fprintf(stderr, "Error mapping memory\n");
+                exit(1);
+            }
+            memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
+            //numa_tonode_memory(JA, (nnzcols + 1) * sizeof(Integer_Type), socket_id);
+        }            
+            
+
         /*
-        #ifdef HAS_WEIGHT
-        if((A = (Weight*) mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(A, 0, nnz * sizeof(Weight));
-        numa_tonode_memory(A, nnz * sizeof(Weight), socket_id);
-        #endif
-        
-        
-        if((IA = (Integer_Type*) mmap(nullptr, nnz * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(IA, 0, nnz * sizeof(Integer_Type));
-        numa_tonode_memory(IA, nnz * sizeof(Integer_Type), socket_id);
-        
-        if((JA = (Integer_Type*) mmap(nullptr, (nnzcols + 1) * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
-        numa_tonode_memory(JA, (nnzcols + 1) * sizeof(Integer_Type), socket_id);
+            if((JC = (Integer_Type*) mmap(nullptr, nnzcols * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+                fprintf(stderr, "Error mapping memory\n");
+                exit(1);
+            }
+            memset(JC, 0, nnzcols * sizeof(Integer_Type));
+            
+            if((IR = (Integer_Type*) mmap(nullptr, nnzrows * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
+                fprintf(stderr, "Error mapping memory\n");
+                exit(1);
+            }
+            memset(IR, 0, nnzrows * sizeof(Integer_Type));
         */
-        /*
-        if((JC = (Integer_Type*) mmap(nullptr, nnzcols * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(JC, 0, nnzcols * sizeof(Integer_Type));
-        
-        if((IR = (Integer_Type*) mmap(nullptr, nnzrows * sizeof(Integer_Type), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
-            fprintf(stderr, "Error mapping memory\n");
-            exit(1);
-        }
-        memset(IR, 0, nnzrows * sizeof(Integer_Type));
-        */
+
     }
 }
 
@@ -360,29 +364,32 @@ template<typename Weight, typename Integer_Type>
 TCSC_BASE<Weight, Integer_Type>::~TCSC_BASE() {
     if(nnz and nnzcols and nnzrows) {
         
-        #ifdef HAS_WEIGHT
-        numa_free(A, (nnz * sizeof(Weight)));
-        #endif
-        numa_free(IA, (nnz * sizeof(Integer_Type)));
-        numa_free(JA, ((nnzcols + 1) * sizeof(Integer_Type)));
+        if(numa_available() != -1) {
+            #ifdef HAS_WEIGHT
+            numa_free(A, (nnz * sizeof(Weight)));
+            #endif
+            numa_free(IA, (nnz * sizeof(Integer_Type)));
+            numa_free(JA, ((nnzcols + 1) * sizeof(Integer_Type)));
+        }
+        else {
+            #ifdef HAS_WEIGHT
+            if(munmap(A, nnz * sizeof(Weight)) == -1) {
+                fprintf(stderr, "Error unmapping memory\n");
+                exit(1);
+            }
+            #endif
+            if(munmap(IA, nnz * sizeof(Integer_Type)) == -1) {
+                fprintf(stderr, "Error unmapping memory\n");
+                exit(1);
+            }
+            
+            if(munmap(JA, (nnzcols + 1) * sizeof(Integer_Type)) == -1) {
+                fprintf(stderr, "Error unmapping memory\n");
+                exit(1);
+            }
+        }
         
-        /*
-        #ifdef HAS_WEIGHT
-        if(munmap(A, nnz * sizeof(Weight)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-        #endif
-        if(munmap(IA, nnz * sizeof(Integer_Type)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-        
-        if(munmap(JA, (nnzcols + 1) * sizeof(Integer_Type)) == -1) {
-            fprintf(stderr, "Error unmapping memory\n");
-            exit(1);
-        }
-        */
+
         /*
         if(munmap(JC, nnzcols * sizeof(Integer_Type)) == -1) {
             fprintf(stderr, "Error unmapping memory\n");
