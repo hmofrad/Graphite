0fc3e81e7b90ce4029c9cb0f568381cf997e2933
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-11 21:54:09 -0500

    Update src/apps/pr1.cpp

--- src/apps/pr1.cpp
+++ src/apps/pr1.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
     bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = true;
-    Tiling_type TT = _2DT_;
+    Tiling_type TT = _2D_COL_;
     Compression_type CT = _TCSC_;
     
     /* Degree execution */
