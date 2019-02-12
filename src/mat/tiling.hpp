834b817c9817348dbb2d666621302b5860a90a6a
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-11 22:48:51 -0500

    Update src/mat/tiling.hpp

--- src/mat/tiling.hpp
+++ src/mat/tiling.hpp
@@ -66,8 +66,12 @@ Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t 
         colgrp_nranks = 1;
         rank_nrowgrps = nranks;
         rank_ncolgrps = 1;
-        //printf("nranks=%d ntiles=%d nrowgrps=%d ncolgrps=%d\n", nranks, ntiles, nrowgrps, ncolgrps);
-        //printf("rank_ntiles=%d rank_nrowgrps=%d rank_ncolgrps=%d\n", rank_ntiles, rank_nrowgrps, rank_ncolgrps);
+    }
+    else if (tiling_type == Tiling_type::_1D_ROW_) {
+        rowgrp_nranks = 1;
+        colgrp_nranks = nranks;
+        rank_nrowgrps = 1;
+        rank_ncolgrps = nranks;
     }
 };
 
