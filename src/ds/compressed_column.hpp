676c0a3e734c9e78b2d421db7194b0676304128c
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-26 17:43:45 -0500

    Update src/ds/compressed_column.hpp

--- src/ds/compressed_column.hpp
+++ src/ds/compressed_column.hpp
@@ -41,8 +41,8 @@ struct Compressed_column {
                               const std::vector<char>&         nnzrows_bitvector,
                               const std::vector<Integer_Type>& nnzrows_indices, 
                               const std::vector<char>&         nnzcols_bitvector,
-                              const std::vector<Integer_Type>& nnzcols_indices,
-                              const bool which) {};                               
+                              const std::vector<Integer_Type>& nnzcols_indices) {};
+                              //const bool which) {};                               
                               
         virtual void populate(const std::vector<struct Triple<Weight, Integer_Type>>* triples,
                               const Integer_Type tile_height, 
@@ -284,8 +284,8 @@ struct TCSC_BASE : public Compressed_column<Weight, Integer_Type> {
                               const std::vector<char>&         nnzrows_bitvector,
                               const std::vector<Integer_Type>& nnzrows_indices, 
                               const std::vector<char>&         nnzcols_bitvector,
-                              const std::vector<Integer_Type>& nnzcols_indices,
-                              const bool which);
+                              const std::vector<Integer_Type>& nnzcols_indices);//,
+                              //const bool which);
                               
         uint64_t nnz;
         Integer_Type nnzcols;
@@ -307,7 +307,7 @@ TCSC_BASE<Weight, Integer_Type>::TCSC_BASE(uint64_t nnz_, Integer_Type nnzcols_,
     if(nnz and nnzcols and nnzrows) {
         //printf("socket_id = %d nnz=%lu\n", socket_id, nnz);
         //send_buffer[i][s_i] = (MessageBuffer*)numa_alloc_onnode( sizeof(MessageBuffer), s_i);
-        
+        /*
         if(numa_available() != -1) {
             #ifdef HAS_WEIGHT
             A = (Weight*) numa_alloc_onnode(nnz * sizeof(Weight), socket_id);
@@ -319,6 +319,7 @@ TCSC_BASE<Weight, Integer_Type>::TCSC_BASE(uint64_t nnz_, Integer_Type nnzcols_,
             memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
         } 
         else {
+            */
             #ifdef HAS_WEIGHT
             if((A = (Weight*) mmap(nullptr, nnz * sizeof(Weight), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == (void*) -1) {    
                 fprintf(stderr, "Error mapping memory\n");
@@ -342,7 +343,7 @@ TCSC_BASE<Weight, Integer_Type>::TCSC_BASE(uint64_t nnz_, Integer_Type nnzcols_,
             }
             memset(JA, 0, (nnzcols + 1) * sizeof(Integer_Type));
             //numa_tonode_memory(JA, (nnzcols + 1) * sizeof(Integer_Type), socket_id);
-        }            
+        //}            
             
 
         /*
@@ -365,7 +366,7 @@ TCSC_BASE<Weight, Integer_Type>::TCSC_BASE(uint64_t nnz_, Integer_Type nnzcols_,
 template<typename Weight, typename Integer_Type>
 TCSC_BASE<Weight, Integer_Type>::~TCSC_BASE() {
     if(nnz and nnzcols and nnzrows) {
-        
+        /*
         if(numa_available() != -1) {
             #ifdef HAS_WEIGHT
             numa_free(A, (nnz * sizeof(Weight)));
@@ -374,6 +375,7 @@ TCSC_BASE<Weight, Integer_Type>::~TCSC_BASE() {
             numa_free(JA, ((nnzcols + 1) * sizeof(Integer_Type)));
         }
         else {
+            */
             #ifdef HAS_WEIGHT
             if(munmap(A, nnz * sizeof(Weight)) == -1) {
                 fprintf(stderr, "Error unmapping memory\n");
@@ -389,7 +391,7 @@ TCSC_BASE<Weight, Integer_Type>::~TCSC_BASE() {
                 fprintf(stderr, "Error unmapping memory\n");
                 exit(1);
             }
-        }
+        //}
         
 
         /*
@@ -413,8 +415,8 @@ void TCSC_BASE<Weight, Integer_Type>::populate(const std::vector<struct Triple<W
                                                const std::vector<char>&         nnzrows_bitvector,
                                                const std::vector<Integer_Type>& nnzrows_indices, 
                                                const std::vector<char>&         nnzcols_bitvector,
-                                               const std::vector<Integer_Type>& nnzcols_indices,
-                                               const bool which) {
+                                               const std::vector<Integer_Type>& nnzcols_indices){
+                                               //const bool which) {
     if(nnz and nnzcols and nnzrows) {
         struct Triple<Weight, Integer_Type> pair;
         Integer_Type i = 0; // Row Index
@@ -423,11 +425,11 @@ void TCSC_BASE<Weight, Integer_Type>::populate(const std::vector<struct Triple<W
         int ii = 0;
         for (auto& triple : *triples) {
             ii++;
-            if(which) {
-                pair  = {triple.row, (triple.col % tile_width)};
+            //if(which) {
+              //  pair  = {triple.row, (triple.col % tile_width)};
                 //pair  = {triple.row - tile_height, (triple.col % tile_width)};
-            }
-            else 
+            //}
+            //else 
                 pair  = {(triple.row % tile_height), (triple.col % tile_width)};
             
             while((j - 1) != nnzcols_indices[pair.col]) {
