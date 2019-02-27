8ad762153358e0752e30c590823e48e1371d2fbc
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-02-26 22:43:43 -0500

    Update src/apps/cc.cpp

--- src/apps/cc.cpp
+++ src/apps/cc.cpp
@@ -27,7 +27,7 @@ int main(int argc, char** argv) {
     bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = false;
-    Tiling_type TT = _2D_COL_;
+    Tiling_type TT = _2DT_;
     // Only CSC is supported for nonstationary algorithms
     Compression_type CT = _TCSC_;
 
