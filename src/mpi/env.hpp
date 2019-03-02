d19bcc9af1f826078939078f204778b40b6cd240
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-03-02 14:00:23 -0500

    Update src/mpi/env.hpp

--- src/mpi/env.hpp
+++ src/mpi/env.hpp
@@ -1,13 +1,12 @@
 /*
  * env.hpp: MPI runtime environment
- * (c) Mohammad Mofrad, 2018
+ * (c) Mohammad Mofrad, 2019
  * (e) m.hasanzadeh.mofrad@gmail.com 
  */
 
 #ifndef ENV_HPP
 #define ENV_HPP
  
-#include <mpi.h>
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -18,12 +17,11 @@
 #include <numeric>
 #include <algorithm>
 
-
-//#ifdef __linux__
-//#include <numa.h>
-//#endif 
+#include <mpi.h>
+#include <omp.h>
+#include <numa.h>
+#include <thread>
 //#include </ihome/rmelhem/moh18/numactl/libnuma/usr/local/include/numa.h>
-//#ifdef NUMA_AVAILABLE 
 
 class Env {
     public:
@@ -34,7 +32,7 @@ class Env {
     static int nranks;
     static bool is_master;
     static void init(bool comm_split_ = true);
-    static void init_t();
+    static void init_threads();
     static void barrier();
     static void finalize();
     static void exit(int code);
@@ -60,12 +58,12 @@ class Env {
     static void   set_comm_split();
     static bool   get_comm_split();
     
-    static char core_name[]; // Core name = hostname
-    static int core_id; // Core id
-    static int nthreads;
-    static int nsockets;
+    static char core_name[]; // Core name = hostname of MPI rank
+    static int core_id;      // Core id of MPI rank
+    static int nthreads;     // Number of threads
+    static int nsockets;     // Number of sockets
     static int nthreads_per_socket;
-    static int nsegments;
+    static int nsegments;    // Number of segments = nranks * nthreads
 };
 
 MPI_Comm Env::MPI_WORLD;
@@ -101,139 +99,35 @@ void Env::init(bool comm_split_) {
     int provided = -1;
     MPI_Init_thread(nullptr, nullptr, required, &provided);
     assert((provided >= MPI_THREAD_SINGLE) && (provided <= MPI_THREAD_MULTIPLE));
-
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
     assert(nranks >= 0);
-    
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     assert(rank >= 0);
-    
     is_master = rank == 0;
-    
     MPI_WORLD = MPI_COMM_WORLD;
-    if(required != provided)
-    {
-        if(is_master)
-        {
-            printf("Failure to set MPI_THREAD_MULTIPLE by MPI (%d)\n", provided); 
-            //printf("Multi-threading is disabled with MPI_THREAD_MULTIPLE (%d/%d)\n", omp_get_num_threads(), omp_get_max_threads());
-        }
+    if(required != provided) {
+        printf("Warning[Rank=%d]: Failure to enable MPI_THREAD_MULTIPLE(%d) for correctness guarantee.\n", rank, provided); 
     }
-    else
-    {
-        if(is_master) {
-            printf("MPI_THREAD_MULTIPLE is set by MPI (%d)\n", provided);
-            //printf("Multi-threading is enabled with MPI_THREAD_MULTIPLE (%d/%d)\n", omp_get_num_threads(), omp_get_max_threads());
-        }
-    }
-    
-    init_t();
+    init_threads();
 }
 
-/*
-inline int get_socket_id(int thread_id) {
-    return thread_id / threads_per_socket;
-  }
-
-inline int get_socket_offset(int thread_id) {
-    return thread_id % threads_per_socket;
-  }
-  */
-
-void Env::init_t() {
-    //if(is_master) {
-        int cpu_name_len;
-        MPI_Get_processor_name(core_name, &cpu_name_len);
-        core_id = sched_getcpu();
-        printf("Rank=%d is running on %s with core id %d\n", Env::rank, core_name, core_id);
-        //nthreads = omp_get_max_threads();
-        nthreads = 7;
-        nsockets = 1;
-        nthreads_per_socket = 1;
-        /*
-        if(numa_available() != -1) {
-        
-        //assert( numa_available() != -1 );
-        //printf("Initializing threads\n");
-            
-            nthreads = numa_num_configured_cpus();
-            nsockets = numa_num_configured_nodes();
-            nsockets = (nsockets) ? nsockets : 1;
-            nthreads_per_socket = nthreads / nsockets;
-            nthreads_per_socket = (nthreads_per_socket) ? nthreads_per_socket : 1;
-            nthreads = omp_get_max_threads();
-            
-            int nodestring_size = nsockets*2+1;
-            char nodestring[nodestring_size];
-            memset(nodestring, 0, nodestring_size);
-            nodestring[0] = '0';
-            for(int i = 1; i < nsockets; i++) {
-              nodestring[i*2-1] = ',';
-              nodestring[i*2] = '0' + i;
-            }
-            
-            //printf("nodestring=%s %d\n", nodestring, nsockets*2+1);
-            struct bitmask * nodemask = numa_parse_nodestring(nodestring);
-            numa_set_interleave_mask(nodemask);   
-            
-            omp_set_dynamic(0);
-            //omp_set_num_threads(nthreads);
-            
-            
-            
-            if(is_master)
-                printf("NUMA-aware OpenMP is enabled with %d threads, %d sockets, and %d threads per socket\n", nthreads, nsockets, nthreads_per_socket);
-        }
-        else {
-            //nthreads = omp_get_max_threads();
-            nthreads = 1;
-            nsockets = 1;
-            nthreads_per_socket = nthreads / nsockets;
-            if(is_master)
-                printf("NUMA-aware OpenMP is disabled with %d threads, %d sockets, and %d threads per socket\n", nthreads, nsockets, nthreads_per_socket);
-        }
-        */
-        nsegments = nranks * nthreads;
-        /*
-        if(is_master) {
-            #pragma omp parallel
-            {
-                int tid = omp_get_thread_num();
-                int cid = sched_getcpu();
-                int sid =  cid / nthreads_per_socket;
-                int sof =  cid % nthreads_per_socket;
-                //int* test = (int*) numa_alloc_onnode(2, sid);
-                //test[0] = tid;
-                printf("Core_name=%s Rank=%d, tid=%d, core_id=%d, sid=%d sof=%d\n", core_name, Env::rank, tid, cid, sid, sof);
-                
-            }
-        }
-        */
-        
-        
-        //for(int i = 0; i < (nsockets*2+1); i++)
-        //    printf("%c ", nodestring[i]);
-        //printf("\n");
-
-        
-        
-        
-    //}
-    
-    /*
-    //int tid, sid;
-    #pragma omp parallel
-    {
-    //for (int i = 0; i < omp_get_max_threads(); i++) {
-        int tid = omp_get_thread_num();
-        int sid = get_socket_id(tid);
-      //assert(numa_run_on_node(s_i)==0);
-      //#ifdef PRINT_DEBUG_MESSAGES
-       printf("thread-%d bound to socket-%d\n", tid, sid);
-      //#endif
+void Env::init_threads() {
+    int cpu_name_len;
+    MPI_Get_processor_name(core_name, &cpu_name_len);
+    core_id = sched_getcpu();
+    nthreads = omp_get_max_threads();
+    if(numa_available() != -1) {
+        nsockets = numa_num_configured_nodes();
+        nsockets = (nsockets) ? nsockets : 1;
+        nthreads_per_socket = numa_num_configured_cpus() / nsockets;
+        nthreads_per_socket = (nthreads_per_socket) ? nthreads_per_socket : 1;
     }
-    */
-
+    else {
+        nsockets = 1;
+        nthreads_per_socket = nthreads / nsockets;
+        printf("Warning[Rank=%d]: Failure to enable NUMA-aware feature\n", rank);
+    }
+    nsegments = nranks * nthreads;
 }
 
 bool Env::get_comm_split() {
