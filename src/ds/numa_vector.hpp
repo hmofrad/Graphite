8abc955650ebc5a8eab39ba9ef7dd167e3cd50da
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-03-18 12:24:26 -0400

    Update src/ds/numa_vector.hpp

--- src/ds/numa_vector.hpp
+++ src/ds/numa_vector.hpp
@@ -8,7 +8,7 @@
 #define NUMA_VECTOR_HPP
 
 template<typename Integer_Type, typename Vector_Type>
-void allocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitems, std::vector<int32_t> socket_ids) {
+void allocate_numa_vector(Vector_Type*** data, const std::vector<Integer_Type> nitems, const std::vector<int32_t> socket_ids, std::vector<uint64_t>& bytes) {
     int32_t vector_length = nitems.size();
     uint64_t nbytes = 0;
     if(numa_available() != -1) {
@@ -19,11 +19,15 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitems,
         for(int32_t i = 0; i < vector_length; i++) {
             if(nitems[i]) {
                 nbytes = nitems[i] * sizeof(Vector_Type);
+                nbytes += Env::L1_CACHE_LINE_SIZE - (nbytes % Env::L1_CACHE_LINE_SIZE);
+                bytes[i] = nbytes;
                 (*data)[i] = (Vector_Type*) numa_alloc_onnode(nbytes, socket_ids[i]);
-                memset((*data)[i], 0, nbytes);    
+                memset((*data)[i], 0, nbytes);  
+                madvise((*data)[i], nbytes, MADV_SEQUENTIAL);                
             }
             else {
                 (*data)[i] = nullptr;
+                bytes[i] = 0;
             }
         }   
     }
@@ -35,11 +39,15 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitems,
         for(int32_t i = 0; i < vector_length; i++) {
             if(nitems[i]) {
                 nbytes = nitems[i] * sizeof(Vector_Type);
+                nbytes += Env::L1_CACHE_LINE_SIZE - (nbytes % Env::L1_CACHE_LINE_SIZE);
+                bytes[i] = nbytes;
                 (*data)[i] = (Vector_Type*) mmap(nullptr, nbytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
                 memset((*data)[i], 0, nbytes);
+                madvise((*data)[i], nbytes, MADV_SEQUENTIAL);
             }
             else {
                 (*data)[i] = nullptr;
+                bytes[i] = 0;
             }
         }
     }
@@ -52,12 +60,13 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitems,
 } 
 
 template<typename Integer_Type, typename Vector_Type>
-void deallocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitems){
+void deallocate_numa_vector(Vector_Type*** data, const std::vector<Integer_Type> nitems, const std::vector<uint64_t> bytes){
     int32_t vector_length = nitems.size();
     uint64_t nbytes = 0;
     if(numa_available() != -1) {
         for(int32_t i = 0; i < vector_length; i++) {
-            nbytes = nitems[i] * sizeof(Vector_Type);
+            //nbytes = nitems[i] * sizeof(Vector_Type);
+            nbytes = bytes[i];
             memset((*data)[i], 0, nbytes);    
             numa_free((*data)[i], nbytes);
         }
@@ -68,7 +77,8 @@ void deallocate_numa_vector(Vector_Type*** data, std::vector<Integer_Type> nitem
     else {
         for(int32_t i = 0; i < vector_length; i++) {
             if(nitems[i]) {
-                nbytes = nitems[i] * sizeof(Vector_Type);
+                //nbytes = nitems[i] * sizeof(Vector_Type);
+                nbytes = bytes[i];
                 memset((*data)[i], 0, nbytes);    
                 munmap((*data)[i], nbytes);
             }
