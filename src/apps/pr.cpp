cd9abc0871e4679781660fa65a38d15f358f928b
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-04-14 21:44:35 -0400

    Update src/apps/pr.cpp

--- src/apps/pr.cpp
+++ src/apps/pr.cpp
@@ -24,35 +24,39 @@ int main(int argc, char** argv) {
     ip num_vertices = std::atoi(argv[2]);
     ip num_iterations = (argc > 3) ? (uint32_t) atoi(argv[3]) : 0;
     bool directed = true;
-    bool transpose = true;
+    bool transpose = false;
     bool self_loops = true;
     bool acyclic = false;
     bool parallel_edges = true;
     Tiling_type TT = _NUMA_;
     Compression_type CT = _TCSC_;
-    
+    Hashing_type HT = _NONE_;
     /* Degree execution */
     Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, HT);
+    
     bool stationary = true;
     bool gather_depends_on_apply = false;
     bool apply_depends_on_iter  = false;
-    Ordering_type OT = _COL_;
+    Ordering_type OT = _ROW_;
     Deg_Program<wp, ip, fp> V(G, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
     V.execute(1);
     V.checksum();
+    G.free_mat();
     Env::barrier();
-    /* Vertex execution */
-    OT = _ROW_;
-    PR_Program<wp, ip, fp> VR(G, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
-    VR.initialize(V);       
+    
+    transpose = true;
+    Graph<wp, ip, fp> GR;    
+    GR.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, HT);
+    PR_Program<wp, ip, fp> VR(GR, stationary, gather_depends_on_apply, apply_depends_on_iter, OT);
+    VR.initialize(V);
     V.free();
-    VR.execute(num_iterations);
+    G.free_graph();
+    VR.execute(num_iterations); // Vertex execution
     VR.checksum();
     VR.display();
     VR.free();
-    G.free();
-    
+    GR.free();    
     double time2 = Env::clock();
     Env::print_time("PageRank end-to-end", time2 - time1);
     Env::finalize();
