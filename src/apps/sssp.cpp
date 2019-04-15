36005373a33600e621c50aff4d47f7e09322c407
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-04-14 21:43:54 -0400

    Update src/apps/sssp.cpp

--- src/apps/sssp.cpp
+++ src/apps/sssp.cpp
@@ -32,14 +32,14 @@ int main(int argc, char** argv) {
     bool parallel_edges = false;
     Tiling_type TT = _NUMA_;
     Compression_type CT = _TCSC_;
-    
+    Hashing_type HT = _NONE_;
     /* Single Source Shortest Path (SSSP) execution*/
     bool stationary = false;
     // Requirement for nonstationary algorithms on directed graphs
     if(not stationary and directed)
         transpose = not transpose; 
     Graph<wp, ip, fp> G;    
-    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT);
+    G.load(file_path, num_vertices, num_vertices, directed, transpose, self_loops, acyclic, parallel_edges, TT, CT, HT);
     bool gather_depends_on_apply = true;
     bool apply_depends_on_iter  = false;
     Ordering_type OT = _ROW_;
