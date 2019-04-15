730973540ac46a81bfb0f378f7f3eb5ffa95a66c
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-04-15 16:38:55 -0400

    Update src/ds/numa_vector.hpp

--- src/ds/numa_vector.hpp
+++ src/ds/numa_vector.hpp
@@ -16,7 +16,6 @@ struct blk {
 
 template<typename Integer_Type, typename Vector_Type>
 void allocate_numa_vector(Vector_Type*** data, std::vector<struct blk<Integer_Type>>& blks) {
-//void allocate_numa_vector(Vector_Type*** data, const std::vector<Integer_Type> nitems, const std::vector<int32_t> socket_ids, std::vector<uint64_t>& bytes) {    
     int32_t vector_length = blks.size();
     uint64_t nbytes = 0;
     uint64_t alignment = 0;
@@ -28,12 +27,10 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<struct blk<Integer_Ty
         for(int32_t i = 0; i < vector_length; i++) {
             auto& blk = blks[i];
             if(blk.nitems) {
-                //nbytes = nitems[i] * sizeof(Vector_Type);
                 nbytes = blk.nitems * sizeof(Vector_Type);
                 alignment = Env::L1_CACHE_LINE_SIZE - (nbytes % Env::L1_CACHE_LINE_SIZE);
                 nbytes += alignment;
                 blk.nbytes = nbytes;
-                //bytes[i] = nbytes;
                 (*data)[i] = (Vector_Type*) numa_alloc_onnode(nbytes, blk.socket_id);
                 memset((*data)[i], 0, nbytes);  
                 madvise((*data)[i], nbytes, MADV_SEQUENTIAL);                
@@ -52,12 +49,10 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<struct blk<Integer_Ty
             auto& blk = blks[i];
             if(blk.nitems) {
                 auto& blk = blks[i];
-                //nbytes = nitems[i] * sizeof(Vector_Type);
                 nbytes = blk.nitems * sizeof(Vector_Type);
                 alignment += (Env::L1_CACHE_LINE_SIZE - (nbytes % Env::L1_CACHE_LINE_SIZE));
                 nbytes += alignment;
                 blk.nbytes = nbytes;
-                //bytes[i] = nbytes;
                 (*data)[i] = (Vector_Type*) mmap(nullptr, nbytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
                 memset((*data)[i], 0, nbytes);
                 madvise((*data)[i], nbytes, MADV_SEQUENTIAL);   
@@ -72,8 +67,6 @@ void allocate_numa_vector(Vector_Type*** data, std::vector<struct blk<Integer_Ty
 
 template<typename Integer_Type, typename Vector_Type>
 void deallocate_numa_vector(Vector_Type*** data, std::vector<struct blk<Integer_Type>> blks){
-//void deallocate_numa_vector(Vector_Type*** data, const std::vector<Integer_Type> nitems, const std::vector<uint64_t> bytes){
-    //int32_t vector_length = nitems.size();
     int32_t vector_length = blks.size();
     uint64_t nbytes = 0;
     if(numa_available() != -1) {
