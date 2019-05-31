b519f194d9b1511e024b60af2c6e7f5c8698dbf2
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-05-31 19:06:35 -0400

    Update src/apps/bfs.cpp

--- src/apps/bfs.cpp
+++ src/apps/bfs.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
     bool parallel_edges = false;
     Tiling_type TT = _NUMA_;
     Compression_type CT = _TCSC_; 
-    Hashing_type HT = _NONE_;
+    Hashing_type HT = _BUCKET_;
     /* Breadth First Search (BFS) execution */
     bool stationary = false;
     // Engine requirement for nonstationary algorithms on directed graphs
