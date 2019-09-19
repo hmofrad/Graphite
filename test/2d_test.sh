3af0bf667b0de6c851e6fc9c34681354c4d101cc
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-09-19 00:14:50 -0400

    Update 2d_test.sh

--- test/2d_test.sh
+++ test/2d_test.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#for i in {1..5}
-#do
-#i=10
-#echo "Hello"
-#doneo
-TILING=$1
-echo "Tiling type test for ${TILING}..."
-for i in {2..1000..1}
+for i in {1..100..1}
 do 
-    ./2d ${TILING}  ${i} 1
-    if [ $? -ne 1 ]
-    then
-	    echo "Test $i failed. ($?)"
-	    #exit
-    else
-	    echo "Test $i passed. ($?)"
-    fi	
-
+    for j in {1..20..2}
+    do	    
+       # j=1
+        ./2d  ${i} ${j}
+        if [ $? -ne 1 ]
+        then
+            echo "Test (${i}, ${j}) failed. ($?)"
+	    exit
+        else
+            echo "Test (${i}, ${j}) passed. ($?)"
+        fi
+   done	
 done
