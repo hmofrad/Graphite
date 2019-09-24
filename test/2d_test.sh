8d3a0c9988925145055d44ada3819ba0227186fa
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-09-24 13:13:45 -0400

    Update 2d_test.sh

--- test/2d_test.sh
+++ test/2d_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+TILING=$1
 for i in {1..1000..1}
 do 
 #    for j in {1..20..2}
  #   do	    
         j=2
-        ./2d  ${i} ${j}
+        ./2d  ${i} ${j} ${TILING}
         if [ $? -ne 1 ]
         then
             echo "Test (${i}, ${j}) failed. ($?)"
