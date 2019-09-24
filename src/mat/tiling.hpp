94972299aa7c5f630e7881018e8d99170b4a3aba
hmofrad <m.hasanzadeh.mofrad@gmail.com>
2019-09-23 22:58:15 -0400

    Update src/mat/tiling.hpp

--- src/mat/tiling.hpp
+++ src/mat/tiling.hpp
@@ -12,8 +12,8 @@
  
 enum Tiling_type {
     //_2DGP_,  // GraphPad
-    _2D_,  // 2D
-    _NUMA_ // 2D with NUMA order
+    _2D_,  // 2DT-Staggered + NUMA
+    _NUMA_ // 2DT-Staggered + NUMA
 };
 
 class Tiling {    
@@ -25,36 +25,67 @@ class Tiling {
     friend class Vertex_Program;
     
     public:    
-        Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_);
+        Tiling(uint32_t nranks_, uint32_t rank_nthreads_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_);
         ~Tiling();
     
     private:
-        uint32_t nranks;
-        uint32_t ntiles, nrowgrps, ncolgrps;
         Tiling_type tiling_type;
+        uint32_t ntiles, nrowgrps, ncolgrps;
+        uint32_t nranks;
         uint32_t rank_ntiles, rank_nrowgrps, rank_ncolgrps;
         uint32_t rowgrp_nranks, colgrp_nranks;
+        uint32_t rank_nthreads;
+        uint32_t nthreads;
+        uint32_t thread_ntiles, thread_nrowgrps, thread_ncolgrps;
+        uint32_t rowgrp_nthreads, colgrp_nthreads;
         void integer_factorize(uint32_t n, uint32_t& a, uint32_t& b);
 };
 
-Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_) {
+Tiling::Tiling(uint32_t nranks_, uint32_t rank_nthreads_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t ncolgrps_, Tiling_type tiling_type_) {
+    tiling_type = tiling_type_;
     nranks = nranks_;
     ntiles = ntiles_;
     nrowgrps = nrowgrps_;
     ncolgrps = ncolgrps_;
     rank_ntiles = ntiles / nranks;
-    tiling_type = tiling_type_;
     assert(rank_ntiles * nranks == ntiles);
     
+    if(not Env::get_init_status()) {
+        Env::shuffle_ranks();
+        if (tiling_type == Tiling_type::_NUMA_) {
+            bool ret = Env::affinity(); // NUMA computation and communication
+        }
+    }
+    integer_factorize(nranks, rowgrp_nranks, colgrp_nranks);
+    assert(rowgrp_nranks * colgrp_nranks == nranks);
+    rank_nrowgrps = nrowgrps / colgrp_nranks;
+    rank_ncolgrps = ncolgrps / rowgrp_nranks;        
+    assert(rank_nrowgrps * rank_ncolgrps == rank_ntiles);
+    
+    integer_factorize(nranks, rowgrp_nranks, colgrp_nranks);
+    assert(rowgrp_nranks * colgrp_nranks == nranks);
+    rank_nrowgrps = nrowgrps / colgrp_nranks;
+    rank_ncolgrps = ncolgrps / rowgrp_nranks;        
+    assert(rank_nrowgrps * rank_ncolgrps == rank_ntiles);
+    
+    printf("%d %d %d\n", Env::nranks, Env::nthreads, Env::nsegments);
+    rank_nthreads = rank_nthreads_;
+    nthreads = nranks * rank_nthreads;   
+    thread_ntiles = ntiles / nthreads;
+    assert(thread_ntiles * nthreads == ntiles); 
+
+    integer_factorize(nthreads, rowgrp_nthreads, colgrp_nthreads);
+    assert(rowgrp_nthreads * colgrp_nthreads == nthreads);
+    thread_nrowgrps = nrowgrps / colgrp_nthreads;
+    thread_ncolgrps = ncolgrps / rowgrp_nthreads;        
+    assert(thread_nrowgrps * thread_ncolgrps == thread_ntiles);
+    
+    /*
     //if ((tiling_type == Tiling_type::_2D_) or (tiling_type == Tiling_type::_2DGP_)) {
     if (tiling_type == Tiling_type::_2D_) {
         if(not Env::get_init_status())
             Env::shuffle_ranks();
-        integer_factorize(nranks, rowgrp_nranks, colgrp_nranks);
-        assert(rowgrp_nranks * colgrp_nranks == nranks);
-        rank_nrowgrps = nrowgrps / colgrp_nranks;
-        rank_ncolgrps = ncolgrps / rowgrp_nranks;        
-        assert(rank_nrowgrps * rank_ncolgrps == rank_ntiles);
+
     }
     else if (tiling_type == Tiling_type::_NUMA_) {
         //rowgrp_nranks = Env::socket_nranks;
@@ -76,6 +107,7 @@ Tiling::Tiling(uint32_t nranks_, uint32_t ntiles_, uint32_t nrowgrps_, uint32_t 
         fprintf(stderr, "ERROR(rank=%d): Invalid tiling type\n", Env::rank);
         Env::exit(1);
     }
+    */
 };
 
 Tiling::~Tiling() {};
